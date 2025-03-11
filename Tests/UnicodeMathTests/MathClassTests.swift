905b8ed850331f5d614792e279cf254769af6f83
Lie Yan <robyn.lie.yan@outlook.com>
2025-03-11 16:02:58 +0800

    chore: upgrade to swift 6

--- Tests/UnicodeMathTests/MathClassTests.swift
+++ Tests/UnicodeMathTests/MathClassTests.swift
@@ -1,18 +1,19 @@
-// Copyright 2024 Lie Yan
+// Copyright 2024-2025 Lie Yan
 
-@testable import UnicodeMathClass
 import XCTest
 
+@testable import UnicodeMathClass
+
 final class MathClassTests: XCTestCase {
-    func testUnicodeMathClass() {
-        XCTAssertEqual(mathClass("0"), MathClass.Normal)
-        XCTAssertEqual(mathClass("a"), MathClass.Alphabetic)
-        XCTAssertEqual(mathClass("𝔸"), MathClass.Alphabetic)
-        XCTAssertEqual(mathClass("+"), MathClass.Vary)
-        XCTAssertEqual(mathClass("×"), MathClass.Binary)
-        XCTAssertEqual(mathClass("("), MathClass.Opening)
-        XCTAssertEqual(mathClass(","), MathClass.Punctuation)
-        XCTAssertEqual(mathClass("|"), MathClass.Fence)
-        XCTAssertEqual(mathClass("😃"), nil)
-    }
+  func testUnicodeMathClass() {
+    XCTAssertEqual(mathClass("0"), MathClass.Normal)
+    XCTAssertEqual(mathClass("a"), MathClass.Alphabetic)
+    XCTAssertEqual(mathClass("𝔸"), MathClass.Alphabetic)
+    XCTAssertEqual(mathClass("+"), MathClass.Vary)
+    XCTAssertEqual(mathClass("×"), MathClass.Binary)
+    XCTAssertEqual(mathClass("("), MathClass.Opening)
+    XCTAssertEqual(mathClass(","), MathClass.Punctuation)
+    XCTAssertEqual(mathClass("|"), MathClass.Fence)
+    XCTAssertEqual(mathClass("😃"), nil)
+  }
 }
